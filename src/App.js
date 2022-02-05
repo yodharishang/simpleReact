86277adcb8c0fbe8a849ba50f7386ddeed8bbd5c
pelatge <pelat.g29@gmail.com>
2022-02-05 22:59:03 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = ({ title }) => <div><p className="text-3xl">{title}</p></div>;
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './assets/css/styles.css';
+
+const title = 'My Minimal React Webpack Nazzgor';
+
+ReactDOM.render(
+  <App title={title} />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
