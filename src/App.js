4d2a33d1d408352bceb424377b852a7781052138
pelatge <pelat.g29@gmail.com>
2022-02-07 01:00:31 +0700

    edit app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './assets/css/styles.css';
-const App = ({ title }) => 
-<div className="">
-  <p className="text-3xl font-bold text-red-600">{title}</p>
-</div>;
+
+import { Blogs,Home,Contact,NoPage} from './pages'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
