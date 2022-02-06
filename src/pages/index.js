c010da9613982392c1ef75b39926b83f91676199
pelatge <pelat.g29@gmail.com>
2022-02-07 01:19:53 +0700

    edit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import { Blogs} from './Blogs.js'
+import { Home } from './Home.js'
+import { Contact} from './Contact.js'
+import { NoPage } from './NoPage.js'
+
+export  { Blogs,Home,Contact,NoPage};
